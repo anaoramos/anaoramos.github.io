c8a3a4a9671d82f8a67e885ecf5ceb23956e9903
anaoramos <anaforamos@gmail.com>
2021-02-16 16:43:07 +0000

    Several changes: - fix vulnerabilities - update professional info - remove outdated cv - update profile pic

--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -1,96 +1,113 @@
-import React from 'react';
-import {makeStyles} from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import {createStyles, Theme} from "@material-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { createStyles, Theme } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import profilePhoto from "../assets/profile.png";
-import RemoveIcon from '@material-ui/icons/Remove';
+import RemoveIcon from "@material-ui/icons/Remove";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexGrow: 1,
-            padding: theme.spacing(2),
-            margin: 'auto',
-            maxWidth: 830,
-        },
-        paper: {
-            padding: theme.spacing(2),
-            margin: 'auto',
-            maxWidth: 600,
-        },
-        image: {
-            width: 400,
-            height: 400,
-        },
-        img: {
-            margin: 'auto',
-            display: 'block',
-            maxWidth: '100%',
-            maxHeight: '100%',
-        },
-        typographyStart: {
-            fontFamily: "monospace",
-            fontWeight: "bolder",
-            color: "slategrey"
-        },
-    }),
+  createStyles({
+    root: {
+      flexGrow: 1,
+      padding: theme.spacing(2),
+      margin: "auto",
+      maxWidth: 830,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      margin: "auto",
+      maxWidth: 600,
+    },
+    image: {
+      margin: "8px",
+      display: "block",
+      maxWidth: "100%",
+      maxHeight: "100%",
+    },
+    typographyTitle: {
+      fontFamily: "monospace",
+      fontWeight: "bolder",
+      color: "slategrey",
+    },
+    typographyText: {
+      fontFamily: "monospace",
+    },
+  })
 );
 
-const infoEnd = (
-    <Typography variant="body2" gutterBottom style={{fontFamily: "monospace"}}>
-        I am a biomedical engineer from Vila do Conde working as a full stack developer.
-        I consider myself a fast, adaptable,and continuous learner who is organized and self-taught.
-        <br/>
-        All my life I haven't been that good at sports, I've tried volleyball, golf, badminton, archery, dancing ... but
-        I wasn't good at all.
-        I've always preferred to go for a walk in nature or near the river and then stop to listen to music or reading
-        a
-        book.
-        Today I still love doing these things. I love reading and listening to music or podcasts.
-        <br/>
-        I have already lived in Braga, Madrid and Lisbon.
-        I really like to know new cities from a touristic point of view or as a local. My next big dream trip is to New
-        York. I really want to see what the big apple looks like and I'm sure I won't have to wait too long for it.
-    </Typography>
-)
+const messages = {
+  nameTitle: "HI, MY NAME IS",
+  name: "Ana Ramos",
+  introduction:
+    "I'm a biomedical engineer turned full stack developer. \
+    I'm a fast learner, adaptable, and self-taught. \
+    Although sports aren't my forte, I enjoy nature walks, reading, and music/podcasts. \
+    I've lived in Braga, Madrid, and Lisbon, and love exploring new cities as a tourist or local.\
+    In my free time, I enjoy building Legos and spending time with my pets. \
+    I'm driven, always seeking growth, and strive to make a positive impact.",
+};
 
 export const PersonalInfo = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <Grid container spacing={1}>
-                <Grid item xs={6} sm container style={{width: '150%'}} alignItems={"center"}>
-                    <Grid item xs container direction="column" spacing={1}>
-                        <Grid item xs>
-                            <div style={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                flexWrap: 'wrap',
-                            }}>
-                                <RemoveIcon fontSize={"large"} style={{color: "slategrey"}}/>
-                                <Typography variant="h5" gutterBottom className={classes.typographyStart}>
-                                    HI, MY NAME IS
-                                </Typography>
-                            </div>
-                            <Typography variant="h1" gutterBottom style={{fontFamily: "monospace"}}>
-                                Ana Ramos
-                            </Typography>
-                        </Grid>
-                        <Grid item xs>
-                            {infoEnd}
-                        </Grid>
-                    </Grid>
-                    <Grid item xs container direction="column" spacing={1} style={{width: 'auto'}}>
-                        <Grid item xs>
-                            <div className={classes.image}>
-                                <img className={classes.img} alt="complex" src={profilePhoto}/>
-                            </div>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </div>
-    );
-}
+  return (
+    <Grid container spacing={1} className={classes.root}>
+      <Grid
+        item
+        xs={6}
+        sm
+        container
+        style={{ width: "150%" }}
+        alignItems="center"
+      >
+        <Grid item xs container direction="column" spacing={1}>
+          <Grid item xs>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                flexWrap: "wrap",
+              }}
+            >
+              <RemoveIcon fontSize="large" style={{ color: "slategrey" }} />
+              <Typography
+                variant="h5"
+                gutterBottom
+                className={classes.typographyTitle}
+              >
+                {messages.nameTitle}
+              </Typography>
+            </div>
+            <Typography
+              variant="h1"
+              gutterBottom
+              className={classes.typographyText}
+            >
+              {messages.name}
+            </Typography>
+          </Grid>
+          <Grid item xs>
+            <Typography
+              variant="body2"
+              gutterBottom
+              className={classes.typographyText}
+            >
+              {messages.introduction}
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          xs
+          container
+          direction="column"
+          spacing={1}
+          style={{ width: "auto" }}
+        >
+          <img className={classes.image} alt="complex" src={profilePhoto} />
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
--- src/components/ProfessionalInfo.tsx
+++ src/components/ProfessionalInfo.tsx
@@ -1,111 +1,90 @@
-import React from 'react';
-import Typography from "@material-ui/core/Typography";
-import Timeline from "@material-ui/lab/Timeline";
-import TimelineItem from "@material-ui/lab/TimelineItem";
-import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
-import TimelineDot from "@material-ui/lab/TimelineDot";
-import TimelineConnector from "@material-ui/lab/TimelineConnector";
-import TimelineContent from "@material-ui/lab/TimelineContent";
-import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent";
-// @ts-ignore
-import MyCV from '../documents/cv.pdf';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import {
+  Timeline,
+  TimelineItem,
+  TimelineOppositeContent,
+  TimelineSeparator,
+  TimelineContent,
+  TimelineDot,
+  TimelineConnector,
+} from "@material-ui/lab";
 
-export const ProfessionalInfo = () => {
-    return (
-        <React.Fragment>
-            <Timeline align="alternate">
-                <TimelineItem>
-                    <TimelineOppositeContent style={{fontSize: "small"}}>
-                        <Typography color="textSecondary" style={{fontSize: "small"}}>
-                            October, 2020
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineDot/>
-                        <TimelineConnector/>
-                    </TimelineSeparator>
-                    <TimelineContent style={{fontSize: "small"}}>
-                        <Typography color="textSecondary" style={{fontSize: "small"}}>
-                            Junior Full Stack Engineer, OTO Systems
-                        </Typography>
-                        <Typography style={{fontSize: "small"}}>
-                            OTO Systems is a spin-off from <b>SRI International</b> that uses artificial intelligence to
-                            analyze speech, processing millions of customer files to help companies, e.g. contact
-                            centers, to help unlock valuable data from voice interactions.
-                            <br/>
-                            I do a variety of tasks like writing internal/user documentation, code, or even support.
-                            <br/>
-                            As  a  full  stack  developer  I’m  involved  in  both  frontend  and  backend  tasks.
-                            In  the  frontend  side, I  develop  frontend  components  using <b>ReactJS</b>,  mostly <b>Typescript</b>.
-                            On  the  backend  side,  I  use <b>Kotlin</b> for editing or adding services for our existing API’s and creating new ones as requested.
-                            <br/>
-                            My engineering team is spread between Portugal and Switzerland, so we plan, track and manage our agile and software development projects with Jira, Zeplin, Slack.
-                            We also use <b>Git</b> as version control system.
-                        </Typography>
-                    </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineOppositeContent style={{fontSize: "small"}}>
-                        <Typography color="textSecondary" style={{fontSize: "small"}}>
-                            January, 2020
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineDot/>
-                        <TimelineConnector/>
-                    </TimelineSeparator>
-                    <TimelineContent style={{fontSize: "small"}}>
-                        <Typography color="textSecondary" style={{fontSize: "small"}}>
-                            Software Engineer Trainee, Nonius Software
-                        </Typography>
-                        <Typography style={{fontSize: "small"}}>
-                            Nonius provides hospitality technology fully integrated with hotel's management systems.
+const useStyles = makeStyles((theme) => ({
+  timelineItem: {
+    marginBottom: theme.spacing(2),
+  },
+  timelineOpposite: {
+    textAlign: "right",
+  },
+  timelineContent: {
+    flex: 1,
+  },
+}));
 
-                            I started a project in which I was responsible for the design of the architecture and the development
-                            of a chatbot to be used by the Nonius support team as well to be integrated in the Nonius Website(performing Human Handoff).
-                            <br/>
-                            My work started by analyzing the chatbot frameworks(cost-effective benefits, required architecture,time of development, ...)
-                            and determining the respective pros and cons. In addition, I also developed different  demos,
-                            using <b>Wit.ai</b> (with <b>Python</b> on  the  server  side  in  combination  with  the <b>Django web framework</b> for  the  frontend)  and  using DialogFlow (with <b>Node.js</b>),
-                            which was originally integrated with Google Assistant.
-                            <br/>
-                            In the end, I worked with DialogFlow framework, using Node.js for the server side.  The frontend was provided by the integration with Tiledesk.
-                        </Typography>
-                    </TimelineContent>
-                </TimelineItem>
-                <TimelineItem>
-                    <TimelineOppositeContent style={{fontSize: "small"}}>
-                        <Typography color="textSecondary" style={{fontSize: "small"}}>
-                            September, 2018
-                        </Typography>
-                    </TimelineOppositeContent>
-                    <TimelineSeparator>
-                        <TimelineDot/>
-                        <TimelineConnector/>
-                    </TimelineSeparator>
-                    <TimelineContent style={{fontSize: "small"}}>
-                        <Typography color="textSecondary" style={{fontSize: "small"}}>
-                            Research Student in Bioeng. and Telemedicine Group, Polytechnic University of Madrid
-                        </Typography>
-                        <Typography style={{fontSize: "small"}}>
-                            Before getting my Masters degree, I decided that it would be good for me to have research
-                            experience to know whether to do a PhD or get a job in industry.
-                            <br/>
-                            That is why I applied to the <b>Bioengineering and Telemedicine Group</b> at Polytechnic
-                            University of Madrid and worked in the <b>artificial intelligence</b> field to investigate
-                            and develop Deep Learning approaches to brain
-                            segmentation of stroke patients using magnetic resonance imaging with no basic truth
-                            designation.
-                            <br/>
-                            It was a good experience to decide that my path now is not to do a PhD, but to work in the
-                            industry.
-                        </Typography>
-                    </TimelineContent>
-                </TimelineItem>
-            </Timeline>
-            <Typography style={{fontSize: "small"}}>
-                You can find more information in <a href={MyCV} download={'ana_ramos_cv.pdf'}>my CV</a>.
+const timelineData = [
+  {
+    date: "August, 2021",
+    title: "Software Engineer, Unity Software",
+    description: `My main focus is to improve safety in the gaming environment. We utilize acoustic and semantic analysis techniques, leveraging Google Speech to Text, to gain valuable insights into the presence of toxic behavior within game voice chats. 
+    This enables us to create a safer and more enjoyable gaming experience for all players involved. 
+    I am actively involved in developing the API using Python and Django, as well as implementing and maintaining a customer dashboard using React and TypeScript.`,
+  },
+  {
+    date: "October, 2020",
+    title: "Full Stack Engineer, OTO Systems",
+    description: `OTO Systems was a spin-off from SRI International that used artificial intelligence to analyze speech, processing millions of customer files to help companies, e.g. contact centers, to help unlock valuable data from voice interactions.
+    As a software engineer, I built a sentiment analysis API and SDK using Python and Django. I also designed and implemented an internal dashboard using React and TypeScript to visualize JSON results and manage organizations, projects, and jobs effectively. 
+    Additionally, I developed a user-friendly sales tool for efficient license key management within the organization. 
+    I contributed to the ongoing maintenance and enhancement of a Kotlin SDK for license key management. 
+    Throughout these projects, I provided dedicated support to users and customers.`,
+  },
+  {
+    date: "January, 2020",
+    title: "Software Engineer Trainee, Nonius Software",
+    description: `At Nonius, a provider of integrated hospitality technology, I led a project focused on designing and developing a chatbot. 
+    The chatbot was intended for use by the Nonius support team and integration into the company's website, enabling seamless human handoff. 
+    My responsibilities included analyzing chatbot frameworks, considering factors like cost-effectiveness, required architecture, and development time. 
+    After careful evaluation, I created demos using Wit.ai and DialogFlow, with the latter ultimately chosen for integration. 
+    I utilized Node.js for the server-side implementation and leveraged the Tiledesk integration for the frontend.`,
+  },
+  {
+    date: "September, 2018",
+    title:
+      "Research Student in Bioeng. and Telemedicine Group, Polytechnic University of Madrid",
+    description: `Prior to pursuing my Masters degree, I sought research experience to determine my career path between pursuing a PhD or entering the industry. 
+    I joined the Bioengineering and Telemedicine Group at Polytechnic University of Madrid, where I focused on artificial intelligence. 
+    Specifically, I investigated and developed Deep Learning techniques for brain segmentation of stroke patients using magnetic resonance imaging, even in the absence of established ground truth.`,
+  },
+];
+
+const ProfessionalInfo = () => {
+  const classes = useStyles();
+
+  return (
+    <Timeline align="alternate">
+      {timelineData.map((item, index) => (
+        <TimelineItem key={index} className={classes.timelineItem}>
+          <TimelineOppositeContent className={classes.timelineOpposite}>
+            <Typography variant="body2" color="textSecondary">
+              {item.date}
             </Typography>
-        </React.Fragment>
-    );
-}
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <TimelineDot />
+            {index !== timelineData.length - 1 && <TimelineConnector />}
+          </TimelineSeparator>
+          <TimelineContent className={classes.timelineContent}>
+            <Typography variant="body2" color="textSecondary">
+              {item.title}
+            </Typography>
+            <Typography variant="body2">{item.description}</Typography>
+          </TimelineContent>
+        </TimelineItem>
+      ))}
+    </Timeline>
+  );
+};
+
+export default ProfessionalInfo;
